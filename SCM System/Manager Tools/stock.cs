cc717886976c770820e124d3b20825510a791b4e
Thomas Starling <starlint@uni.coventry.ac.uk>
2017-11-25 12:31:38 +0000

    Minor change

--- SCM System/Manager Tools/stock.cs
+++ SCM System/Manager Tools/stock.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace SCM_System.Manager_Tools
 {
@@ -13,23 +14,159 @@ namespace SCM_System.Manager_Tools
 
         private void searchBtn_Click(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\v11.0; AttachDbFilename =| DataDirectory |\database.mdf; Integrated Security = True; Connect Timeout = 30"))
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;Connect Timeout=30"))
             {
+                if(txtID.Text != String.Empty)
+                {
+                    connection.Open();
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM Stock WHERE (Id = '" + txtID.Text + "' AND status = '" + statusDropDown.selectedValue + "')", connection);
+                        SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) FROM Stock WHERE (Id = '" + txtID.Text + "')", connection);
+                        int result = (int)cmdCheck.ExecuteScalar();
 
+                        if (result > 0)
+                        {
+                            dataGridView1.Visible = true;
+                            bunifuThinButton22.Visible = true;
+
+                            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                            {
+                                using (DataSet ds = new DataSet())
+                                {
+                                    sda.Fill(ds);
+                                    dataGridView1.DataSource = ds.Tables[0];
+                                }
+                            }
+                            connection.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record exists with that information");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unexpected error:" + ex.Message);
+                    }
+                }
+                else if(txtName.Text != String.Empty)
+                {
+                    connection.Open();
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM Stock WHERE (name = '" + txtName.Text + "' AND status = '" + statusDropDown.selectedValue + "')", connection);
+                        SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) FROM Stock WHERE (name = '" + txtName.Text + "')", connection);
+                        int result = (int)cmdCheck.ExecuteScalar();
+
+                        if (result > 0)
+                        {
+                            dataGridView1.Visible = true;
+                            bunifuThinButton22.Visible = true;
+
+                            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                            {
+                                using (DataSet ds = new DataSet())
+                                {
+                                    sda.Fill(ds);
+                                    dataGridView1.DataSource = ds.Tables[0];
+                                }
+                            }
+                            connection.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record exists with that information");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unexpected error:" + ex.Message);
+                    }
+                }
             }
         }
 
-        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        private void bunifuThinButton21_Click_1(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\v11.0; AttachDbFilename =| DataDirectory |\database.mdf; Integrated Security = True; Connect Timeout = 30"))
+            if(groupBoxAddNew.Visible == true)
             {
+                using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;Connect Timeout=30"))
+                {
+                    connection.Open();
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand(@"INSERT INTO Stock ([Id], [name], [price], [quantity], [arrival], [minimum], [maximum], [staffCheck], [status]) VALUES (@productID,@productName, @productPrice, @productQuantity, @productArrival, @productMin, @productMax, @productStaff,  @productStatus);", connection);
+                        cmd.Parameters.AddWithValue("@productID", textBoxID.Text);
+                        cmd.Parameters.AddWithValue("@productName", textBoxName.Text);
+                        cmd.Parameters.AddWithValue("@productPrice", textBoxPrice.Text);
+                        cmd.Parameters.AddWithValue("@productQuantity", textBoxQuan.Text);
+                        cmd.Parameters.AddWithValue("@productArrival", datePickerArrival.Value);
+                        cmd.Parameters.AddWithValue("@productMin", textBoxMin.Text);
+                        cmd.Parameters.AddWithValue("@productMax", textBoxMax.Text);
+                        cmd.Parameters.AddWithValue("@productStaff", textBoxSignOff.Text);
+                        cmd.Parameters.AddWithValue("@productStatus", bunifuDropdownStatus.selectedValue);
 
+                        int i = cmd.ExecuteNonQuery();
+
+                        if (i == 1)
+                        {
+                            MessageBox.Show("Product has being added");
+                            try
+                            {
+                                cmd = new SqlCommand("SELECT * FROM Stock WHERE (Id = '" + textBoxID.Text + "')", connection);
+                                SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) FROM Stock WHERE (Id = '" + textBoxID.Text + "')", connection);
+                                int result = (int)cmdCheck.ExecuteScalar();
+
+                                if (result > 0)
+                                {
+                                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                                    {
+                                        using (DataSet ds = new DataSet())
+                                        {
+                                            sda.Fill(ds);
+                                            dataGridView1.DataSource = ds.Tables[0];
+                                        }
+                                    }
+
+                                    dataGridView1.Visible = true;
+                                    bunifuThinButton22.Visible = true;
+                                    connection.Close();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("No record exists with that information");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Unexpected error:" + ex.Message);
+                            }
+                            dataGridView1.Visible = true;
+                            groupBoxAddNew.Visible = false;
+                            connection.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record exists with that information");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unexpected error:" + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                dataGridView1.Visible = false;
+                groupBoxAddNew.Visible = true;
             }
         }
 
-        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        private void bunifuThinButton22_Click_1(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\v11.0; AttachDbFilename =| DataDirectory |\database.mdf; Integrated Security = True; Connect Timeout = 30"))
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;Connect Timeout=30"))
             {
 
             }

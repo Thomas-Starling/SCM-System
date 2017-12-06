dd5d1ddef548e05cae92bd45cc6a21bed5923dcb
Thomas Starling <starlint@uni.coventry.ac.uk>
2017-12-06 16:35:42 +0000

    Minor changes

--- SCM System/Mediator/Submission/stockControlMediator.cs
+++ SCM System/Mediator/Submission/stockControlMediator.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace SCM_System.Mediator
 {
     public delegate void SignOffEvent(String id, String n, String s, DataGridView d);
-    public delegate void StockArrival(String id, String n, String s, DataGridView d);
-    public delegate bool ValidateStock(String id, String n, String s, SqlConnection c, DataGridView d);
-    public delegate void EnterItemDetails(String id, String n, String s, DataGridView d);
-    public delegate bool ValidateOrder(String id, String n, String s, SqlConnection c, DataGridView d);        
-    public delegate bool SignOffOrder(String id, String n, String s, SqlConnection c, DataGridView d);
-    public delegate bool UpdateOrders(String id, String n, String s, SqlConnection c, DataGridView d);
+    public delegate void StockArrival(String id);
+    public delegate bool ValidateStock(String id);
+    public delegate void EnterItemDetails(String id, String n);
+    public delegate bool ValidateOrder(String id);        
+    public delegate bool SignOffOrder(String id);
+    public delegate bool UpdateOrders(String id);
     public delegate void NotifyManager(bool r);
 
     class signOffMediator
@@ -25,7 +25,7 @@ namespace SCM_System.Mediator
 
         public managerMediator()
         {
-            this.signOff += new OrderManagmentEvent(this.signOffEventHandler);
+            this.signOff += new SignOffEvent(this.signOffEventHandler);
         }
 
         public void signOffEventHandler(String id, String n, String s, DataGridView d)
@@ -43,12 +43,12 @@ namespace SCM_System.Mediator
 
     class Mediator
     {
-        public event EnterItem item;
-        public event ValidateItem validate;
-        public event PerformAction action;
-        public event ValidateAction valAction;
-        public event NotifyManager signOff;
-        public event UpdateItem update;
+        public event StockArrival item;
+        public event ValidateStock validate;
+        public event EnterItemDetails action;
+        public event ValidateOrder valAction;
+        public event SignOffOrder signOff;
+        public event UpdateOrders update;
         public event NotifyManager notify;
 
         private String ID, name, status;
@@ -65,39 +65,155 @@ namespace SCM_System.Mediator
             this.notify += new NotifyManager(this.notificationProcessor);
         }
 
-        public voids stockPrcoessor(String id, String n, DataGridView d)
+        public void stockPrcoessor(String id, DataGridView d){ }
+
+        public bool stockValidationProcessor(String id, DataGridView d)
         {
-            
+            openConnection(c);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Stock WHERE (Id = '" + id + "')", c);
+                int result = (int)cmd.ExecuteScalar();
+
+                if (result > 0)
+                {
+                    closeConnection(c);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:" + ex.Message);
+                return false;
+            }
         }
 
-        public voids stockValidationProcessor(String id, String n, DataGridView d)
+        public void checkProcessor(String id, String n, DataGridView d)
         {
-
+            if (id == String.Empty && n == String.Empty) //Check if textbox empty
+            {
+                this.notify("Search Critera is missig");
+            }
         }
 
-        public voids checkProcessor(String id, String n, DataGridView d)
+        public void validateOrderProcessor(String id, String n, DataGridView d)
         {
+            openConnection(c);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Orders WHERE (Id = '" + id + "')", c);
+                int result = (int)cmd.ExecuteScalar();
 
+                if (result > 0)
+                {
+                    closeConnection(c);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:" + ex.Message);
+                return false;
+            }
         }
 
-        public voids validateOrderProcessor(String id, String n, DataGridView d)
+        public void signOffProcessor(String id, String n, DataGridView d)
         {
+            openConnection(c);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE Stock SET Arrival = True WHERE (Id = '" + id + "')", c);
+                int result = (int)cmd.ExecuteScalar();
 
+                if (result > 0)
+                {
+                    closeConnection(c);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:" + ex.Message);
+                return false;
+            }
         }
 
-        public voids signOffProcessor(String id, String n, DataGridView d)
+        public void updateProcessor(String id, String n, DataGridView d)
         {
+            openConnection(c);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Stock WHERE (Id = '" + id + "')", c);
+                int result = (int)cmd.ExecuteScalar();
 
+                if (result > 0)
+                {
+                    this.display(cmd, d);
+                    closeConnection(c);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:" + ex.Message);
+                return false;
+            }
         }
 
-        public voids updateProcessor(String id, String n, DataGridView d)
+        public void notificationProcessor(bool i)
         {
-
+            if (i)
+            {
+                MessageBox.Show("Record Found, Select Field...");
+            }
+            else
+            {
+                MessageBox.Show("No Record Exists");
+            }
         }
 
-        public voids notificationProcessor(bool i)
+        public static bool openConnection(SqlConnection c)
         {
-            
+            try
+            {
+                c.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
+            }
+        }
+
+        public static bool closeConnection(SqlConnection c)
+        {
+            try
+            {
+                c.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
+            }
         }
     }
 }
